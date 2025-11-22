fb5b27597778ff9f71e05117f8a52f7759899815
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-22 21:21:19 +0400

    in main.jsx added Provider and put the app inside of it so that we can use redux innside App anywhere

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//შემოგვაქვს Provider და ჩვენი store
+import { Provider } from 'react-redux'
+import {store} from './redux/store.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}> {/*ვფუთავთ მთლიან აპლიკაციას Provider-ში */}
     <App />
+    </Provider>
   </StrictMode>,
 )
