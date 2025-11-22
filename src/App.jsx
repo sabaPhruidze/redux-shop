6a083708ef1281efb42da2f2a12f5d7ec828c19b
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-22 22:19:52 +0400

    logic has some issue I have to fix button click it shows 12.5 when I only pressed an apple

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
+import React from 'react';
 import { useSelector,useDispatch } from "react-redux"
 import { addToCart,clearCart } from "./redux/cartSlice"
+import { ShoppingCart,Trash2 } from "lucide-react";
 
+ const products = [
+    {id:1, name:'მარწყვი',price:2.5},
+    {id:2, name:'ვაშლი', price:3.5},
+    {id:3, name:'ბანანი', price: 4.6},
+    {id:4, name:'ანანასი', price : 9.8}
+  ]
 function App() {
   // ვიღებთ მთლიან state-ს და იქიდან მხოლოდ ჩვენს cart-ს
-  const cartData = useSelector((state) => state.cart)
+  const {cartItems, totalPrice} = useSelector((state) => state.cart)
   //ვქმნით კურიერს
   const dispatch = useDispatch();
-  const banana = { id: 1, name: 'ბანანი', price: 3 };
+ 
 
   return (
-    <div>
-     <h1>  სულ გადასახდელია: {cartData.totalPrice} </h1>
-     <button onClick={() => dispatch(addToCart(banana))}> {/* ვეუბნები რომ გაუშვას ბრძანება რომლითაც addToCart რომელიც შევქმენით slice ში ღილაკად მიიღოს და დაამუშაოს banana */}
-      ბანანის ყიდვა
-     </button>
-     <button onClick={() => dispatch(clearCart())}>
-      კალათის გასუფთავება
-     </button>
+    <div className='app-container'>
+      <nav className='navbar'>
+        <h1>Fresh Market</h1>
+        <div className='cart-badge'>
+          <ShoppingCart size={24}/>
+          <span>{cartItems.length}</span>
+          <span className='price-tag'>{totalPrice.toFixed(1)}</span> {/*used toFixed method from prototype */}
+
+        </div>
+      </nav>
+      <main className='main-content'>
+       <h2>აირჩიე სასურველი პროდუქტი</h2>
+       <div className='products-grid'>
+         {products.map((product) => (
+          <div key={product.id} className='product-card'>
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+            <button className='add-button' onClick={() => dispatch(addToCart(product))}>
+              დამატება
+            </button>
+          </div>
+        ))}
+       </div>
+       {cartItems.length > 0 && (
+        <div className='cart-actions'>
+          <button onClick={() => dispatch(clearCart())} className="clear-button">გაათავისუფლე კალათი</button>
+          <Trash2 size={18}/>
+        </div>
+       )}
+      </main>
     </div>
   )
 }
