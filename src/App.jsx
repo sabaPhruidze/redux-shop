2e067206982d5544040bbf00a69c6a30a45831f4
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-22 21:53:42 +0400

    So I imported 2 keys addToCart and clearCart from cartSlice where we created it and than used dispatch which is like an order of giving information to concreate button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useSelector } from "react-redux"
+import { useSelector,useDispatch } from "react-redux"
+import { addToCart,clearCart } from "./redux/cartSlice"
 
 function App() {
   // ვიღებთ მთლიან state-ს და იქიდან მხოლოდ ჩვენს cart-ს
   const cartData = useSelector((state) => state.cart)
+  //ვქმნით კურიერს
+  const dispatch = useDispatch();
+  const banana = { id: 1, name: 'ბანანი', price: 3 };
 
   return (
     <div>
-       სულ გადასახდელია: {cartData.totalPrice}
+     <h1>  სულ გადასახდელია: {cartData.totalPrice} </h1>
+     <button onClick={() => dispatch(addToCart(banana))}> {/* ვეუბნები რომ გაუშვას ბრძანება რომლითაც addToCart რომელიც შევქმენით slice ში ღილაკად მიიღოს და დაამუშაოს banana */}
+      ბანანის ყიდვა
+     </button>
+     <button onClick={() => dispatch(clearCart())}>
+      კალათის გასუფთავება
+     </button>
     </div>
   )
 }
